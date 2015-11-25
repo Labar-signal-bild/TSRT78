ce3d29833635a3859652e7bd035d6948966d8417
Anna Hjelmberg <annhj876@student.liu.se>
2015-11-25 13:26:28 +0100

    added poole_stable

--- Lab I/Lab1.m
+++ Lab I/Lab1.m
@@ -19,6 +19,14 @@ N = 16000;
 X = fft(x);
 Xsamp = length(X);
 
+Xplot = fftshift(fft(x));
+
+figure(1);clf(); 
+plot(-8000/2:0.5:(8000-1)/2, abs(Xplot))
+xlabel('Frequency, Hz')
+axis([-2000 2000 0 60])
+
+
 % 1: Energy time domain 
 Etot1 = sum(abs(x).^2); % Definition of the energy in the time domain
 
@@ -28,10 +36,10 @@ x_t = filter(B,A,x);
 
 Edom1 = sum(abs(x_t).^2);
 
-figure(10)
-plot(1:N,abs(fft(x_t)))
-figure(11)
-plot(1:N,abs(fft(x)))
+%figure(10)
+%plot(1:N,abs(fft(x_t)))
+%figure(11)
+%plot(1:N,abs(fft(x)))
 
 
 
@@ -59,7 +67,7 @@ harmonic2 = 1-Edom2/Etot2;
 %% 4: AR
 
 
-mo = ar(x,2); % AR model of order 2
+mo = ar(x,2, [],[],[],1/8000); % AR model of order 2
 A = mo.a;
 [R,P,K] = residue(1,A); 
 
@@ -69,14 +77,12 @@ distance = 1-placement; % The distance from the unit circle to the pole
 
 %% 5: Estimate the dominant frequancy paratertic/non-parametric
 
-% One of them should be a bode plot. Which one?? Both?
-
 spect_val = etfe(x, 100, [], 1/8000); % The non-parametric method
 
 figure(1); bode(mo, 'r', spect_val, 'b');
 legend('AR-model', 'Original Signal');
 
-
+print(1,'bode1.eps','-depsc','-loose');
 
 %%
 %------------------------------------------------------------------------------------------------------
@@ -140,8 +146,10 @@ mo9 = ar(edata, 9);
 mo30 = ar(edata, 30);
 mo20 = ar(edata, 20);
 
-figure(2); bode(mo4, 'r', mo7, 'g', mo8, 'b', mo20, 'm', aa_spect, 'y');
-legend(' = mo4', ' = mo7', ' = mo8', ' = mo20', ' = validation');
+figure(2); bode(mo7, 'g', mo8, 'b', mo20, 'm', aa_spect, 'y');
+legend(' = mo7', ' = mo8', ' = mo20', ' = validation', 'Location', 'SouthWest');
+
+print(2,'bodeval.eps','-depsc','-loose');
 
 %%
 
@@ -152,6 +160,8 @@ Ts=40;
 fs = 7970;
 N = fs*2;
 pulselength = 100;
+
+
 
 pulsetrain = zeros(1,N);
 
@@ -272,11 +282,11 @@ for i=1:200
     y(160*(i-1)+1:160*(i)) = detrend(yhat(i,:));
 end
 
-y=y./(mean(abs(y)));
+y=y./(20*(mean(abs(y))));
 
 figure(4); plot(y);
 mean(abs(y))
-%sound(y,8000)
+sound(y,8000)
 %%
 Y = fft(y);
 
